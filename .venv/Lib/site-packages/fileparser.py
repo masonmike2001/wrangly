1d07a41e00c994bd47ee58a12f932998ff8aac93
Michael Mason <masonmike2001@gmail.com>
2024-09-13 15:27:04 -0400

    change to single level directory parsing, add favorites and directory changing features

---
+++ .venv/Lib/site-packages/fileparser.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+class FileParser(object):
+    pass
--- parser/main.py
+++ parser/main.py
@@ -1,24 +1,64 @@
+import os
 from sys import argv
 
-from parser import parser
+from parser import parser, parse_directory
 
 
 def main():
-    # imports command line argument for directory
+    favorite_paths = []
+    wd = argv[1]
     while True:
         print("**********************************************")
-        print("Welcome to Wrangly! Please select an option:")
-        print("(1) Traverse the directory and begin a session")
-        print("(2) Restore a previous session")
-        print("(3) Exit without saving")
+        print("Welcome to Wrangly:")
+
+        print("(1) Set favorite directories")
+        print("(2) Traverse the directory and begin a session")
+        print("(3) Choose another directory to organize")
+        print("(4) Exit")
         print("**********************************************")
         choice = int(input("Selection: "))
         if choice == 1:
-            parser(argv[1])
+            # files holds the directories
+            files = parse_directory(wd, True)
+            k = 0
+            for file in files:
+                print(f"({k + 1}) {file}")
+                k += 1
+            print("(0) Return")
+            selection = -1
+            while selection != 0:
+                selection = int(input("Which of these directories would you like to favorite: "))
+                if selection == 0:
+                    break
+                else:
+                    favorite_paths.append(files[int(selection) - 1])
+                    print(f"{files[int(selection) - 1]} has been favorited")
+                    print(favorite_paths)
         elif choice == 2:
-            # To do
-            return
+            parser(wd, favorite_paths)
         elif choice == 3:
+            print("Please type in the directory you'd like to organize"
+                  "level higher.")
+            files = parse_directory(wd, True)
+            k = 0
+            for file in files:
+                print(f"({k + 1}) {file}")
+                k += 1
+            print("(-1) Go up one directory <../>")
+            print("(0) Return")
+            selection = -1
+            while selection != 0:
+                selection = int(input("Selection: "))
+                if selection == 0:
+                    break
+                elif selection == -1:
+                    wd = (wd.rsplit("\\", 1))[0]
+                    break
+                else:
+                    wd = files[int(selection)]
+                    print(f"{files[int(selection) - 1]} is the new path. Returning...")
+                    break
+        elif choice == 4:
             return
         else:
             print("Invalid choice. Please try again.")
